c52d3de5049e8330160a016b60dc5cdad22a2c7c
Drew Cook <drewcook29@gmail.com>
2021-05-07 11:06:10 -0600

    use api key

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,49 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt-dadjokes',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'nuxt-dadjokes',
+		htmlAttrs: {
+			lang: 'en',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+	],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    headers: {
-      Accept: 'application/json',
-      'x-rapidapi-key': 'f0446d0bc6msh173fbd17b0b705ap11d1a1jsn66abe53a4f40',
-      'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
-      useQueryString: true,
-    },
-  },
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {
+		headers: {
+			Accept: 'application/json',
+			'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+			'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
+			useQueryString: true,
+		},
+	},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 }
